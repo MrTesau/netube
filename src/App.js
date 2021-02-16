97822e37e9c2f6e721c7609b0dbac58c21c0c049
Tayyib Esau <esamu161@gmail.com>
2021-02-16 16:58:00 +1300

    css tidy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import * as ROUTES from "./routes";
 // Front End Layout:
 // Pages > Containers > Components > styled MarkUp
